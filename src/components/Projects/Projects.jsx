3806ff78f263f32de46e6f1f3b6211218795184a
LebogangMputle <lebozikalala@gmail.com>
2024-07-02 10:49:17 +0200

    "Updated Projects component CSS and JSX files to refactor styling and layout"

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,23 +1,29 @@
 // import React from 'react';
 import './Projects.css';
 
-const projects = () => {
+const Projects = () => {
   return (
     <section id="projects" className="projects">
-      <h1>projects</h1>
-      <p>201: HTS Rustenburg
-      </p>
-      <p>Johannesburg Institude of Engineering And Technology
-        <ul>
-          <p>Subjects:</p>
-          <li>Mathematics N5</li>
-          <li>Power Machines N5</li>
-          <li>Electrotechnics</li>
-          <li>Industrial Electronichs</li>
-        </ul>
-      </p>
+      <h1>Projects</h1>
+      <div className="projects-grid">
+        <div id='project-1' className='project-card'>
+          <ul>
+            <li>Podcast App</li>
+          </ul>
+        </div>
+        <div id='project-2' className='project-card'>
+          <ul>
+            <li>Legacy-Redefined</li>
+          </ul>
+        </div>
+        <div id='project-3' className='project-card'>
+          <ul>
+            <li>Project 3 goes here</li>
+          </ul>
+        </div>
+      </div>
     </section>
   );
 };
 
-export default projects;
+export default Projects;
